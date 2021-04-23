b42cfa8de0b06f16e4962dfdd63cd95ade36f795
Tay0108 <kjkosecki@gmail.com>
2021-04-23 22:55:24 +0200

    feat: validating location endpoints :boom:

--- src/route/location/patch.ts
+++ src/route/location/patch.ts
@@ -1,12 +1,16 @@
 import { LocationService } from "../../service/LocationService";
 import { dbConfig } from "../../model";
+import { NotFoundError } from "../../errors/NotFoundError";
+import { BadRequestError } from "../../errors/BadRequestError";
+import { body, param } from "express-validator";
+import { validateRequest } from "../../service/RequestValidationService";
 
 const debug = require("debug")("location.patch");
 
 const locationService = new LocationService();
 
 const locationRoutes = {
-  patch: async (req, res) => {
+  patch: async (req, res, next) => {
     const locationId = req.params.id;
 
     let location;
@@ -17,7 +21,7 @@ const locationRoutes = {
           (await locationService.getById(locationId, transaction)) !== null;
 
         if (!locationFound) {
-          throw new Error("Location not found");
+          throw new NotFoundError("Location not found");
         }
 
         await locationService.update(
@@ -29,12 +33,28 @@ const locationRoutes = {
       });
     } catch (error) {
       debug(error);
-      return res.sendStatus(500);
+      next(error);
     }
     return res.json(location);
   }
 };
 
 export const patchLocation = (app) => {
-  app.patch("/location/:id", locationRoutes.patch);
+  app.patch(
+    "/location/:id",
+    [
+      param("id", "Id has to be number.").isInt(),
+      body().custom((body) => {
+        if (Object.entries(body).length === 0 && body.constructor === Object) {
+          throw new BadRequestError("Request has empty body.");
+        }
+        return Promise.resolve();
+      }),
+      body("name", "Name cannot be empty.").optional().not().isEmpty(),
+      body("latitude", "Latitude has to be a number.").optional().isNumeric(),
+      body("longitude", "Longitude has to be a number.").optional().isNumeric()
+    ],
+    validateRequest,
+    locationRoutes.patch
+  );
 };
